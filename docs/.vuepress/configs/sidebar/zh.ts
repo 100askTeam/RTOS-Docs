304b395693b3c426a7644f650c565cac2533c287
myj020329 <mengyingjie@example.com>
2023-09-06 21:00:29 +0800

    docs(freeRTOS): add chapter16

--- docs/.vuepress/configs/sidebar/zh.ts
+++ docs/.vuepress/configs/sidebar/zh.ts
@@ -22,6 +22,7 @@ export const sidebarZh: SidebarConfig = {
         '/zh/freeRTOS/DShanMCU-F103/chapter13.md',
         '/zh/freeRTOS/DShanMCU-F103/chapter14.md',
         '/zh/freeRTOS/DShanMCU-F103/chapter15.md',
+        '/zh/freeRTOS/DShanMCU-F103/chapter16.md',
       ],
     },
   ],
